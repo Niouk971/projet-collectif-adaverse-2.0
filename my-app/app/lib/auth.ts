9c114f92ea4fb0508d6a29c53f011096cfe4af20
Niouk971 <florian.polter.77@gmail.com>
2026-01-21 01:32:55 +0100

    fix code for vercel deployment

--- my-app/app/lib/auth.ts
+++ my-app/app/lib/auth.ts
@@ -5,6 +5,7 @@ import * as schema from "@/app/lib/db/schema"; // Change l'import en fonction
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
     emailAndPassword: {
         enabled: true, // On active les comptes par email et mot de passe
     },
